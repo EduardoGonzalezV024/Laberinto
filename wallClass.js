8fd2fb24f2890c09547f5103e7b58dba609cfc71
EduardoGonzalezV024 <lalo024_gv@hotmail.com>
2023-09-15 12:33:28 -0700

    maze nodes division

--- wallClass.js
+++ wallClass.js
@@ -1,26 +1,32 @@
-class Wall {
-    constructor(x, y, w, h, img, up, down, right, left) {
+class Node {
+    constructor(x, y, w, h) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-        this.img = img;
-        this.up
+        this.up;
+        this.down;
+        this.right;
+        this.left;
     }
 
     paint(){
-        ctx.beginPath();
-        if(this.img == null){
-            ctx.fillRect (this.x, this.y, this.w, this.h);
-        }
-        else{
-            window.onload = ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
-        }
-    }
-
-    strokePaint(){
+        ctx.fillStyle = "rgb(255,255,255)";
         ctx.beginPath();
         ctx.fillRect (this.x, this.y, this.w, this.h);
-        ctx.strokeRect (this.x, this.y, this.w, this.h);
     }
+
+
+
+    setUp(up) {this.up = up;}
+    getUp(){return this.up;}
+
+    setDown(down) {this.down = down;}
+    getDown(){return this.down;}
+
+    setRight(right) {this.right = right;}
+    getRight(){return this.right;}
+
+    setLeft(left) {this.left = left;}
+    getLeft(){return this.left;}
 }
