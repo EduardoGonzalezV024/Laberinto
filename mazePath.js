7068166b520b10e5b4159b50ebb72ae383cd88a4
EduardoGonzalezV024 <lalo024_gv@hotmail.com>
2023-09-16 10:18:08 -0700

    walls and path tracing beginning

---
+++ mazePath.js
@@ -0,0 +1,37 @@
+class Node {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.up;
+        this.down;
+        this.right;
+        this.left;
+        this.visited = false;
+        this.wallLeft = new Wall(this.x-2, this.y-2, 2, this.h+2);
+        this.wallUp = new Wall(this.x-2, this.y-2, this.w+2, 2);
+    }
+
+    paint(){
+        ctx.fillStyle = "rgb(0,0,0)";
+        ctx.fillRect (this.wallLeft.x, this.wallLeft.y, this.wallLeft.w, this.wallLeft.h);
+        ctx.fillRect (this.wallUp.x, this.wallUp.y, this.wallUp.w, this.wallUp.h);
+    }
+
+    visited(){
+        this.visited = true;
+    }
+
+    setUp(up) {this.up = up;}
+    getUp(){return this.up;}
+
+    setDown(down) {this.down = down;}
+    getDown(){return this.down;}
+
+    setRight(right) {this.right = right;}
+    getRight(){return this.right;}
+
+    setLeft(left) {this.left = left;}
+    getLeft(){return this.left;}
+}
--- wallClass.js
+++ wallClass.js
@@ -1,32 +1,14 @@
-class Node {
+class Wall {
     constructor(x, y, w, h) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-        this.up;
-        this.down;
-        this.right;
-        this.left;
     }
 
     paint(){
-        ctx.fillStyle = "rgb(255,255,255)";
+        ctx.fillStyle = "rgb(0,0,0)";
         ctx.beginPath();
         ctx.fillRect (this.x, this.y, this.w, this.h);
     }
-
-
-
-    setUp(up) {this.up = up;}
-    getUp(){return this.up;}
-
-    setDown(down) {this.down = down;}
-    getDown(){return this.down;}
-
-    setRight(right) {this.right = right;}
-    getRight(){return this.right;}
-
-    setLeft(left) {this.left = left;}
-    getLeft(){return this.left;}
 }
