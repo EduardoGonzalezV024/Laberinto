770c6ca643009e506dd86f1a968bf03a15a05e82
EduardoGonzalezV024 <lalo024_gv@hotmail.com>
2023-09-17 12:06:14 -0700

    procedural back tracing maze generation

--- mazePath.js
+++ mazePath.js
@@ -8,20 +8,22 @@ class Node {
         this.down;
         this.right;
         this.left;
+        this.thisCell;
         this.visited = false;
         this.wallLeft = new Wall(this.x-2, this.y-2, 2, this.h+2);
         this.wallUp = new Wall(this.x-2, this.y-2, this.w+2, 2);
     }
 
     paint(){
+        ctx.fillStyle = "rgb(200,200,200)";
+        ctx.fillRect (this.x, this.y, this.w, this.h);
         ctx.fillStyle = "rgb(0,0,0)";
         ctx.fillRect (this.wallLeft.x, this.wallLeft.y, this.wallLeft.w, this.wallLeft.h);
         ctx.fillRect (this.wallUp.x, this.wallUp.y, this.wallUp.w, this.wallUp.h);
     }
 
-    visited(){
-        this.visited = true;
-    }
+    visited(){this.visited = true;}
+    getVisited(){return this.visited;}
 
     setUp(up) {this.up = up;}
     getUp(){return this.up;}
@@ -34,4 +36,7 @@ class Node {
 
     setLeft(left) {this.left = left;}
     getLeft(){return this.left;}
+
+    setThisCell(thisCell) {this.thisCell = thisCell;}
+    getThisCell(){return this.thisCell;}
 }
