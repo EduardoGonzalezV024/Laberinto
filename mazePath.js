39de5f3822da710c121479ea7e98f739d531e0ac
EduardoGonzalezV024 <lalo024_gv@hotmail.com>
2023-09-17 20:21:03 -0700

    AAAAAAAHHHHHHHHHHHHH!!!!!!!!

--- mazePath.js
+++ mazePath.js
@@ -12,14 +12,29 @@ class Node {
         this.visited = false;
         this.wallLeft = new Wall(this.x-2, this.y-2, 2, this.h+2);
         this.wallUp = new Wall(this.x-2, this.y-2, this.w+2, 2);
+        this.floor = new Image();
     }
 
     paint(){
+        ctx.beginPath();
         ctx.fillStyle = "rgb(200,200,200)";
         ctx.fillRect (this.x, this.y, this.w, this.h);
-        ctx.fillStyle = "rgb(0,0,0)";
-        ctx.fillRect (this.wallLeft.x, this.wallLeft.y, this.wallLeft.w, this.wallLeft.h);
-        ctx.fillRect (this.wallUp.x, this.wallUp.y, this.wallUp.w, this.wallUp.h);
+        
+        if(this.wallLeft != null){
+            ctx.fillStyle = "rgb(0,0,0)";
+            ctx.fillRect (this.wallLeft.x, this.wallLeft.y, this.wallLeft.w, this.wallLeft.h);
+        }else{
+            ctx.fillStyle = "rgba(200,200,200,0)";
+            ctx.fillRect (this.x-2, this.y-2, 2, this.h+2);
+        }
+
+        if(this.wallUp != null){
+            ctx.fillStyle = "rgb(0,0,0)";
+            ctx.fillRect (this.wallUp.x, this.wallUp.y, this.wallUp.w, this.wallUp.h);
+        }else{
+            ctx.fillStyle = "rgba(200,200,200,0)";
+            ctx.fillRect (this.x, this.y-2, this.w, 2);
+        }
     }
 
     visited(){this.visited = true;}
